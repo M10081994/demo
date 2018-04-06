7bbf5a9c4ae104dacbc98b967c6a1536408aa778
M10081994 <33995959+M10081994@users.noreply.github.com>
2018-04-06 12:53:48 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,21 @@
+// Java program to Reverse a String using ListIterator
+import java.lang.*;
+import java.lang.reflect.Array;
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    	 try {
+    	      System.exit(0);
+    	    } finally {
+    	      System.err.println("I'm not dead yet!");
+    	      System.exit(1);
+    	    }
+    	  
+    }
+    
+}
